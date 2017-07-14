368f1d50114231ee1aee0dcd9f787126106dcbba
Akshay Jagatap <akshayjagatap@gmail.com>
2017-07-14 18:17:42 +0530

    Create tsne.py

---
+++ gsoc2017-akshay/tsne.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+X = np.loadtxt("embeddings");
+# print(X[0])
+labels = []
+with open('labels') as output:
+	for line in output:
+		labels.append(line.split('resource/')[1].strip())
+
+tsne = TSNE(n_components=2, random_state=0)
+Y = tsne.fit_transform(X)
+
+fig, ax = plt.subplots()
+ax.scatter(Y[:,0], Y[:,1]);
+
+for i, txt in enumerate(labels):
+	ax.annotate(txt, (Y[i,0], Y[i,1]))
+plt.show()
