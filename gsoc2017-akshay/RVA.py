3cde790c1d833eea18f8b5cf91689ace863e7860
Akshay Jagatap <akshayjagatap@gmail.com>
2017-07-24 01:19:24 +0530

    using sparse vectors

--- gsoc2017-akshay/RVA.py
+++ gsoc2017-akshay/RVA.py
@@ -4,33 +4,64 @@ import re
 import numpy as np
 import os
 from multiprocessing import Process, Manager, Pool
+from random import random
 import time
-# window = 6
-# dim = 600
-# q = 1./120
+
+def rand(minimum, maximum):
+    return minimum + (maximum - minimum) * random()
+
+class Sparse:
+	def __init__(self, dim, count, limit):
+		self.dim = dim
+		self.sparse = {}
+		for i in range(count):
+			self.sparse[int(rand(0, dim))] = rand(-limit, limit)
+#dim = dimension of vector
+#count = number of non-zero values
+#limit = range of the non-zero values
+
+	def value(self):
+		a = []
+		for i in range(self.dim):
+			try:
+				a.append(self.sparse[i])
+			except:
+				a.append(0)
+		return a
+
+def add(a, b, weight=1):
+	c = a
+	for i in b.sparse:
+		try:
+			c.sparse[i] += (weight * b.sparse[i])
+		except:
+			c.sparse[i] = (weight * b.sparse[i])
+	return c
+
+
 
 def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, ' ', raw_html)
-    return cleantext
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, ' ', raw_html)
+	return cleantext
 
 class MySentences(object):
-    def __init__(self, dirname):
-        self.dirname = dirname
+	def __init__(self, dirname):
+		self.dirname = dirname
 
-    def __iter__(self):
-        punct = '!"#$%&\'()*+,.:;<=>?@[\\]^`{|}~'
-        for root, dirs, files in os.walk(self.dirname):
-            for filename in files:
-                file_path = root + '/' + filename
-                for line in open(file_path):
-                    sline = line.strip()
-                    if sline == "":
-                        continue
-                    if sline.startswith('<doc'):
-                        sline = 'title/resource/' + sline.split('title="')[1].split('">')[0].replace(' ', '_')
-                    rline = cleanhtml(sline)
-                    yield re.sub(r'[%s]' % punct, '', rline).lower().split()
+	def __iter__(self):
+		punct = '!"#$%&\'()*+,.:;<=>?@[\\]^`{|}~'
+		for root, dirs, files in os.walk(self.dirname):
+			for filename in files:
+				file_path = root + '/' + filename
+				for line in open(file_path, encoding="utf-8"):
+					sline = line.strip()
+					if sline == "":
+						continue
+					if sline.startswith('<doc'):
+						sline = 'title/resource/' + sline.split('title="')[1].split('">')[0].replace(' ', '_')
+					rline = cleanhtml(sline)
+					yield re.sub(r'[%s]' % punct, '', rline).lower().split()
 
 #using numpy
 # def randomVector(num):
@@ -38,11 +69,6 @@ class MySentences(object):
 #   return choice([0, 1], size=num, p=[1 - q, q])
 
 # generate sparse random vectors fast using time.time()
-def randomVector(dim):
-    rv = np.zeros(dim)
-    for i in range(10):
-        rv[int(time.time()*10000000)%dim] = (time.time()*10000000)%10
-    return rv
 
 #using fastrand
 # def randomVector(dim):
@@ -51,92 +77,84 @@ def randomVector(dim):
 #       rv[fastrand.pcg32bounded(dim)] = fastrand.pcg32bounded(5)
 #   return rv
 
-def add(vec1, vec2, weight=1):
-    return np.add(vec1, weight * vec2)
-
 def generateEmbeddings(index, embeddings, sentence, title):
-    dim = 5000#vector dimens
-    window = 2#widnow for context words
-    try:
-        index[title]
-    except:
-        index[title] = randomVector(5000)
+	dim = 500#vector dimens
+	window = 2#window for context words
+	count = 2#number of non-zero values
+	limit = 5#range of non-zero values
+	try:
+		index[title]
+	except:
+		index[title] = Sparse(dim, count, limit)
 
-    if len(sentence) >= window:
-        for i in range(len(sentence) - window):
-            if sentence[i].startswith("resource/"):
-                #add index vector of title entity
-                try:
-                    embeddings[sentence[i]] = add(embeddings[sentence[i]], index[title], 3)
-                except:
-                    # embeddings[sentence[i]] = np.zeros(dim)
-                    embeddings[sentence[i]] = 3 * index[title]
+	if len(sentence) >= window:
+		for i in range(len(sentence) - window):
+			if sentence[i].startswith("resource/"):
+				#add index vector of title entity
+				try:
+					embeddings[sentence[i]] = add(embeddings[sentence[i]], index[title], 3)
+				except:
+					embeddings[sentence[i]] = Sparse(dim, 0, 1)
+					embeddings[sentence[i]] = add(embeddings[sentence[i]], index[title], 3)
 
-                #neighbouring words
-                for j in range(int(i - window/2), i):#left context
-                    try:
-                        embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
-                    except:
-                        index[sentence[j]] = randomVector(dim)
-                        embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
-                for j in range(i + 1, int(i + (window/2) + 1)):#right context
-                    try:
-                        embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
-                    except:
-                        index[sentence[j]] = randomVector(dim)
-                        embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
+				#neighbouring words
+				for j in range(int(i - window/2), i):#left context
+					try:
+						embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
+					except:
+						index[sentence[j]] = Sparse(dim, count, limit)
+						embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
+				for j in range(i + 1, int(i + (window/2) + 1)):#right context
+					try:
+						embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
+					except:
+						index[sentence[j]] = Sparse(dim, count, limit)
+						embeddings[sentence[i]] = add(embeddings[sentence[i]], index[sentence[j]])
 
-
-    # print("Processed ", str(wc), " words.", end="\r")
-            # print(sentence[i] + '(' + str(embeddings[sentence[i]]) + ')')
-            # print(sentence[i], end=' ')
+	# print("Processed ", str(wc), " words.", end="\r")
+			# print(sentence[i] + '(' + str(embeddings[sentence[i]]) + ')')
+			# print(sentence[i], end=' ')
 
 if __name__ == '__main__':
-    directory = sys.argv[1]
-    # embeddings = {}
-    # index = {}
-    sentences = MySentences(directory)
-    manager = Manager()
-    embeddings = manager.dict()
-    index = manager.dict()
-    wc = 0
-    title = ''
+	directory = sys.argv[1]
+	sentences = MySentences(directory)
+	manager = Manager()
+	embeddings = manager.dict()
+	index = manager.dict()
+	wc = 0
+	title = ''
+	now = time.time()
 
-    # proc = [Process(target=generateEmbeddings, args=(index, embeddings, file)) for file in filenames]
-    # for p in proc: p.start()
-    # for p in proc: p.join()
-    now = time.time()
+	pool = Pool()
+	for sentence in sentences:
+		wc += len(sentence)
+		if len(sentence) > 0 and sentence[0].startswith('title/'):
+			title = sentence[0].split('title/')[1]
+			print(sentence)
+		else:
+			pool.apply_async(generateEmbeddings, args=(index, embeddings, sentence, title))
+	pool.close()
+	pool.join()
 
-    pool = Pool()
-    for sentence in sentences:
-        wc += len(sentence)
-        if len(sentence) > 0 and sentence[0].startswith('title/'):
-            print(sentence)
-            title = sentence[0].split('title/')[1]
-        else:
-            pool.apply_async(generateEmbeddings, args=(index, embeddings, sentence, title))
-    pool.close()
-    pool.join()
+	print("Processed ", str(wc), " words.")
 
-    print("Processed ", str(wc), " words.")
+	print("Time elapsed: ", str(time.time() - now), 's')
 
-    print("Time elapsed: ", str(time.time() - now), 's')
+	embeddings = dict(embeddings)
+	index = dict(index)
 
-    embeddings = dict(embeddings)
-    index = dict(index)
+	with open('embeddings', 'w+', encoding="utf-8") as output:
+		with open('labels', 'w+', encoding="utf-8") as op:
+			for word in embeddings:
+				# output.write(word + ' ==')
+				op.write(word + '\n')
+				for i in embeddings[word].value():
+					output.write(' ' + str(i))
+				output.write('\n')
 
-    with open('embeddings', 'w+') as output:
-        with open('labels', 'w+') as op:
-            for word in embeddings:
-                # output.write(word + ' ==')
-                op.write(word + '\n')
-                for i in embeddings[word]:
-                    output.write(' ' + str(i))
-                output.write('\n')
-
-    with open('index', 'w+') as output:
-        for word in index:
-            output.write(word + ' ==')
-            for i in index[word]:
-                output.write(' ' + str(int(i)))
-            output.write('\n')
+	with open('index', 'w+', encoding="utf-8") as output:
+		for word in index:
+			output.write(word + ' ==')
+			for i in index[word].value():
+				output.write(' ' + str(int(i)))
+			output.write('\n')
