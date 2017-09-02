0b8e9b258d3cd65c23925b4e698ca7007cadd2b4
Nausheen Fatma <fatma.nausheen@gmail.com>
2017-09-02 08:58:32 +0530

    Update dbpedia_run.py

--- gsoc2017-nausheen/complex/dbpedia_run.py
+++ gsoc2017-nausheen/complex/dbpedia_run.py
@@ -11,7 +11,7 @@ if __name__ =="__main__":
 	start_time = time.time()
 	print(start_time)
 	#Load data, ensure that data is at path: 'path'/'name'/[train|valid|test].txt
-	dbpediaexp = build_data(name = 'dbpedia_1million_dataset',path = tools.cur_path + '/datasets/')
+	dbpediaexp = build_data(name = 'dbpedia_dataset',path = tools.cur_path + '/datasets/')
 
 
 	#SGD hyper-parameters:
@@ -25,9 +25,9 @@ if __name__ =="__main__":
 
 	#Here each model is identified by its name, i.e. the string of its class name in models.py
 	#Parameters given here are the best ones for each model, validated from the grid-search described in the paper
-	#all_params = { "Complex_Logistic_Model" : params } ; emb_size = 100; lmbda =0.1; params.learning_rate=0.5
+	all_params = { "Complex_Logistic_Model" : params } ; emb_size = 100; lmbda =0.1; params.learning_rate=0.5
 	all_params = { "DistMult_Logistic_Model" : params } ; emb_size = 100; lmbda =0.01
-	#all_params = { "TransE_L1_Model" : params } ; emb_size = 100; lmbda =1.0 ; params.neg_ratio=1; params.learning_rate=0.1
+	all_params = { "TransE_L1_Model" : params } ; emb_size = 100; lmbda =1.0 ; params.neg_ratio=1; params.learning_rate=0.1
 
 
 
@@ -43,6 +43,4 @@ if __name__ =="__main__":
 	print("--- %s seconds ---" % (time.time() - start_time))
 	#Print best averaged metrics:
 	dbpediaexp.print_best_MRR_and_hits()
-	print("--- %s saving embeddings ---")
-	dbpediaexp.save_embeddings(model_s="DistMult_Logistic_Model",filename="DBpedia_DistMult_embeddings.txt")
-	print("--- %s embeddings saved---")
+
