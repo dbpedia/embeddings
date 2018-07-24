f0f9553574a22fefd83857ad0c6c817337b37723
bharat.suri <bsuri97@gmail.com>
2018-07-24 16:00:00 +0530

    Update sparql.py

--- gsoc2018-bharat/src/sparql.py
+++ gsoc2018-bharat/src/sparql.py
@@ -1,6 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import asyncio
-
+from urllib.error import HTTPError
+from SPARQLWrapper.SPARQLExceptions import EndPointInternalError
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 final_pool = []
@@ -8,9 +9,9 @@ query = """
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX dbo: <http://dbpedia.org/ontology/>
     SELECT DISTINCT ?s ?abstract WHERE {
-        ?s a/rdfs:subClassOf* dbo:Organisation .
-        ?s dbo:abstract ?abstract .
-        filter(langMatches(lang(?abstract), "en"))
+    ?s a/rdfs:subClassOf* dbo:Organisation .
+    ?s dbo:abstract ?abstract .
+    filter(langMatches(lang(?abstract), "en"))
     }
     LIMIT 10000 OFFSET """
 
@@ -19,21 +20,26 @@ query = """
 
 async def fetch_data(q, i):
     """
-    Async call to DBpedia, making sparql queries
-    to get the abstracts of entities of type 'Organisation'
-
-    Arguments
-    ---------
-    q : query to be made
-    i : request id
-    """
+        Async call to DBpedia, making sparql queries
+        to get the abstracts of entities of type 'Organisation'
+        
+        Arguments
+        ---------
+        q : query to be made
+        i : request id
+        """
     global final_pool
-    print(f'Sending request # {i + 1}')
+    print(f'INFO : sending request # {i + 1}')
     sparql.setQuery(q)
     sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
+    try:
+        results = sparql.query().convert()
+    except (HTTPError, EndPointInternalError) as _:
+        fetch_data(q, i)
+    
     data = results['results']['bindings']
     final_pool = final_pool + data
+    print(f'INFO : completed request # {i + 1}')
 
 
 futures = [fetch_data(query + str(i * 10000), i) for i in range(0, 20)]
