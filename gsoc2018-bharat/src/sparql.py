29bf8c1119d44202dcbb62223409aa73a3a999d9
Bharat Suri <bsuri97@gmail.com>
2018-07-26 18:37:40 +0530

    Update sparql.py

--- gsoc2018-bharat/src/sparql.py
+++ gsoc2018-bharat/src/sparql.py
@@ -2,20 +2,25 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import asyncio
 from urllib.error import HTTPError
 from SPARQLWrapper.SPARQLExceptions import EndPointInternalError
+import json
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 final_pool = []
+
 query = """
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX dbo: <http://dbpedia.org/ontology/>
-    SELECT DISTINCT ?s ?abstract WHERE {
+    SELECT DISTINCT ?s ?abstract ?url ?p
+    WHERE {
         ?s a/rdfs:subClassOf* dbo:Organisation .
+        ?s foaf:isPrimaryTopicOf ?url ;
+            a ?p .
+        ?p a owl:Class .
         ?s dbo:abstract ?abstract .
         filter(langMatches(lang(?abstract), "en"))
     }
-    LIMIT 10000 OFFSET """
-
-# queries = [query + str(i * 10000) for i in range(0, 5)]
+LIMIT 10000 OFFSET """
 
 
 async def fetch_data(q, i):
@@ -42,7 +47,26 @@ async def fetch_data(q, i):
     print(f'INFO : completed request # {i + 1}')
 
 
-futures = [fetch_data(query + str(i * 10000), i) for i in range(0, 2)]
+futures = [fetch_data(query + str(i * 10000), i) for i in range(0, 50)]
 loop = asyncio.get_event_loop()
 loop.run_until_complete(asyncio.wait(futures))
-print(len(final_pool))
+count = 0
+with open('data/dbpedia_dictionary.json', 'w+') as abstract_dictionary:
+    with open('data/classes_dictionary.json', 'w+') as class_dictionary:
+        for line in final_pool:
+            print('Abstracts : {0}'.format(count), end='\r')
+            a = {}
+            c = {}
+            entity = ''
+            t = ''
+            entity = line['s']['value']
+            entity = entity.replace('http://dbpedia.org/resource/', '')
+            entity = entity.lower()
+            a[entity] = line['abstract']['value'].lower()
+            t = line['p']['value']
+            c[entity] = t.replace('http://dbpedia.org/ontology/', '')
+            json.dump(a, abstract_dictionary)
+            abstract_dictionary.write('\n')
+            json.dump(c, class_dictionary)
+            class_dictionary.write('\n')
+            count += 1
