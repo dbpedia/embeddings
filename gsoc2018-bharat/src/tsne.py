918dca3ab2f6ad001ba7d79b2bb1010bad154a15
bharat.suri <bsuri97@gmail.com>
2018-07-31 03:29:59 +0530

    PEP 8 styling

--- gsoc2018-bharat/src/tsne.py
+++ gsoc2018-bharat/src/tsne.py
@@ -23,9 +23,9 @@ tsne = TSNE(n_components=2, random_state=0)
 Y = tsne.fit_transform(X)
 
 fig, ax = plt.subplots()
-ax.scatter(Y[:,0], Y[:,1])
+ax.scatter(Y[:, 0], Y[:, 1])
 
 for i, txt in enumerate(labels):
-    ax.annotate(txt, (Y[i,0], Y[i,1]))
+    ax.annotate(txt, (Y[i, 0], Y[i, 1]))
 
 plt.show()
