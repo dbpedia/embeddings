75e85038b4e6a9902133aac2216dbf74f42d9d18
Bharat Suri <bsuri97@gmail.com>
2018-08-06 22:49:49 +0530

    Update create_db.py

--- gsoc2018-bharat/src/create_db.py
+++ gsoc2018-bharat/src/create_db.py
@@ -1,6 +1,5 @@
 import json
 from gensim.models import FastText
-from gensim.models import KeyedVectors
 import torch
 from torch.autograd import Variable
 import numpy as np
@@ -12,8 +11,6 @@ import sys
 logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
 logging.root.level = logging.INFO
 model = torch.load('model/description_encoder')
-# wv = KeyedVectors.load_word2vec_format('model/glove.6B.100d.txt.word2vec',
-#                                        binary=False)
 m = FastText.load('model/entity_fasttext_n100')
 wv = m.wv
 del m
@@ -27,12 +24,9 @@ def main(model, wv, abstracts_file, db_file):
         with open(db_file, 'w+') as output_file:
             for line in input_file:
                 print('INFO : {0}'.format(count), end='\r')
-                # if count % 1000 == 0:
-                #     logging.info(f'reading resource # {count}')
                 db = {}
                 json_line = json.loads(line)
                 target = [_ for _ in json_line.keys()][0]
-                # d = json_line[target]
                 desc = json_line[target]['abstract']['value']
                 desc = desc.translate(table).lower().split()
                 if len(desc) > 10:
@@ -54,13 +48,6 @@ def main(model, wv, abstracts_file, db_file):
                     json.dump(db, output_file)
                     output_file.write('\n')
                     count += 1
-                    # print('Entity : ' + target)
-                    # print('Abstract : ' + d, end='')
-                    # print('Predicted : ', end='')
-                    # print(wv.similar_by_vector(p))
-                    # output_file.write(target + '\n')
-                    # output_file.write(d)
-                    # output_file.write(str(p) + '\n')
                 else:
                     continue
 
